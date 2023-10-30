f867eb09d7f7c4b318cb227e289abb4eaa2212f8
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2023-10-30 23:04:25 +0530

    new changes

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ setTimeout(getData, 2500)
 function getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
-  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  title.innerHTML = 'Virat Kohli Is My Ideal '
   excerpt.innerHTML =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+    'A spunky, chubby teenager with gelled hair  shot to fame after leading India to glory in the Under-19 World Cup at Kuala Lumpur in early 2008. In an Indian team filled with saint-like icons w...'
   profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
-  name.innerHTML = 'John Doe'
-  date.innerHTML = 'Oct 08, 2020'
+    '<img src="https://i.pinimg.com/474x/f5/1d/0a/f51d0a59def3381376fbbb99fb7c4825.jpg" alt="virat" />'
+  name.innerHTML = 'Virat Kohli'
+  date.innerHTML = 'Nov 05, 1988'
 
   animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
   animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
